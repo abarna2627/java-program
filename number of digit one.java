16a9435ef5c895359bc58e1d4947a01e15cf02a8
abarna2627 <abarna2007a@gmail.com>
2026-09-21 17:15:42 +0530

    Add files via upload

---
+++ number of digit one.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countDigitOne(int n) {
+      int count=0;
+      for(long i=1;i<=n;i *=10){
+        long a=n/i;
+        long b=n%i;
+        count+=(a+8)/10*i;
+        if(a%10==1)
+        count+=b+1;
+
+      }  
+    return count;
+}
+}
