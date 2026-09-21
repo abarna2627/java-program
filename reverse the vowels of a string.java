d9e607923f3b3767c3e30d502ae762f52a0f0b51
abarna2627 <abarna2007a@gmail.com>
2026-09-21 18:44:52 +0530

    Add files via upload

---
+++ reverse the vowels of a string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String reverseVowels(String s) {
+        char[] a=s.toCharArray();
+        int i=0,j=a.length-1;
+        while(i<j){
+            while(i<j && !vowel(a[i])) i++;
+            while(i<j && !vowel(a[j])) j--;
+            char t=a[i];
+            a[i]=a[j];
+            a[j]=t;
+            i++;
+            j--;
+        }
+        return new String(a);
+    
+}
+boolean vowel(char c){
+    return "aeiouAEIOU".indexOf(c) != -1;
+}
+}
