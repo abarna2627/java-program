b65e55d27ba2848d6780891ee68a9ceb0cc4c95a
abarna2627 <abarna2007a@gmail.com>
2026-09-21 17:36:31 +0530

    Add files via upload

---
+++ rotate list.java
@@ -0,0 +1,16 @@
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || head.next==null)
+        return head;
+        for(int i=0;i<k;i++){
+            ListNode temp=head;
+            while(temp.next.next != null)
+            temp=temp.next;
+            ListNode last=temp.next;
+            temp.next=null;
+            last.next=head;
+            head=last;
+        }
+        return head;
+    }
+    }
