485dd32613c819494dc93544e6e5e802968cd8bf
abarna2627 <abarna2007a@gmail.com>
2026-09-22 17:17:06 +0530

    Add files via upload

---
+++ reverse integer.java
@@ -0,0 +1,13 @@
+lass Solution {
+    public int reverse(int x) {
+        int rev=0;
+        
+    while(x!=0){
+        int r=x%10;
+        rev=rev*10+r;
+        x=x/10;
+    }
+    return rev;
+    
+        }
+    }
