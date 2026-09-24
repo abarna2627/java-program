5cb666383585d99f92d8e8e89b3d309d34ecb2ab
abarna2627 <abarna2007a@gmail.com>
2026-09-24 08:41:24 +0530

    Add files via upload

---
+++ longest palindrome.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int count[]=new int[128];
+        for(char c:s.toCharArray())
+         count[c]++;
+         int ans=0;
+         for(int n:count){
+            ans+=(n/2)*2;
+            if(ans%2==0 && n%2==1)
+            ans++;
+         }
+         return ans;
+    }
+}
